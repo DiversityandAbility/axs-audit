6c4601bd94c0eebf199f2067961ffe04ecb60374
Ashley <ashley.fijalkowska@diversityandability.com>
2023-10-12 16:36:59 +0100

    G127 added into 2.4.2 as advisory

--- audit/criteria/c_2_4_2.py
+++ audit/criteria/c_2_4_2.py
@@ -2,6 +2,7 @@ from audit.criteria.base import Criteria, Level
 from audit.results import CriteriaResult, ResultType
 from audit.techniques.general.G88 import G88
 from audit.techniques.html.H25 import H25
+from audit.techniques.general.G127 import G127
 
 
 class C_2_4_2(Criteria):
@@ -11,6 +12,8 @@ class C_2_4_2(Criteria):
     summary = "Web pages have titles that describe topic or purpose."
 
     def test(self, page, context):
+        yield from G127().test(page, context, type=ResultType.ADVISORY)
+
         res = list(G88().test(page, context, type=ResultType.SUFFICIENT))
         res.extend(H25().test(page, context, type=ResultType.SUFFICIENT))
         yield from res
